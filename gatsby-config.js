a4f4b82056e4a06290fcf738087f081fe1e16344
Nataly Merezhuk <hello@nataly.dev>
2021-06-18 09:24:23 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  siteMetadata: {
+    title: "Nataly Jazz Violin",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-shopify",
+      options: {
+        shopName: "nataly-jazz-violin",
+        accessToken: "6b525d78f501bf1c0b52e80f3b480935",
+      },
+    },
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
